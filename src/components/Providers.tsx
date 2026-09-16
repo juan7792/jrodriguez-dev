98641a8a9bdf0755857658188d6b1b1082221fd3
Juan <j.rodriguez.lvv@gmail.com>
2026-09-16 10:28:40 +0200

    providers: setting dark mode as default theme

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       {children}
     </ThemeProvider>
   );
